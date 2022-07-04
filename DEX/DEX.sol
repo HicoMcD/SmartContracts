b3adcf17d14b99dfe2dd803559670fa58923ba4e
Hico <70553434+HicoMcD@users.noreply.github.com>
2022-07-04 20:11:30 +0200

    Started with DEX

--- DEX/DEX.sol
+++ DEX/DEX.sol
@@ -2,18 +2,28 @@
 
 pragma solidity ^0.8.13;
 
-// Imports OZ
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
-    // Libraries
-    
-    // Variables
+// Interface
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
+
+// Variables
+    uint public reserve0;
+    uint public reserve1;
+
+    uint totalSupply;
+    mapping(address => uint) public balanceOf;
 
     // Events
 
     // Constructor to initiate contract
-    constructor() {
-        
+    constructor(address _token0, address _token1) {
+        token0 = IERC20(_token0);
+        token1 = IERX20(_token1);
     }
+
+    
 
 }
--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Building Smart Contracts
 1) NFT with Royalties - Incorporated IEP-2981 NFT Royalty Standard 
     - OnGoing: Testing 
     - ToDo: Incorporate Rarible NFT Royalties
-2) Multi-Sig Wallet - Multi-Signature Wallet
-3) DEX 
+2) Multi-Sig Wallet - Multi-Signature Wallet (Credit to solidity-by-example)
+3) DEX - Constant Product AMM (Credit to solidity-by-example)
